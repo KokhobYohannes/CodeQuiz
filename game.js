55fd32d7069725aa5b8d8d693a5e0aa4eba34a16
Kokhob Yohannes <kokhobyohannes@yahoo.com>
2020-12-14 19:20:02 -0500

    Fixed additional syncing issues

--- game.js
+++ game.js
@@ -67,7 +67,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/endgame.html')
+        return window.location.assign('./endgame.html')
     }
 
     questionCounter++
